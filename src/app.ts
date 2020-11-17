597b8cb3bb8eb60b3d1aea7866842b4376164fb0
Dan Vanderkam <danvk@sidewalklabs.com>
2020-11-17 16:09:53 -0500

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import * as fs from 'fs';
+import swaggerUI from 'swagger-ui-express';
+import {TypedRouter, apiSpecToOpenApi} from 'typed-router';
+
+import {API} from './api';
+import * as movies from './movies';
+
+const apiSchema = JSON.parse(fs.readFileSync('./src/api.schema.json', 'utf8'));
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(apiSpecToOpenApi(apiSchema)));
+
+const typedRouter = new TypedRouter<API>(app, apiSchema);
+
+movies.register(typedRouter);
+
+app.listen(4567, () => {
+  console.log(`App is running at http://localhost:4567`);
+});
